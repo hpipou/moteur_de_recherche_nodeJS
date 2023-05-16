ddc99f567473c4dfb4c3a96e57f577388ed77f58
hpipou <mr.aoudjit@gmail.com>
2023-05-16 14:25:10 +0200

    SCRAPPING FROM WEBSITE LIST & SAVE IT TO BDD

---
+++ index.js
@@ -0,0 +1,62 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const mysql = require('mysql');
+const webSiteList = require("./listeWebSite")
+require('dotenv').config();
+
+// Création de la connexion à la base de données
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+// Fonction pour effectuer le web scraping d'un site web
+async function scrapeWebsite(website) {
+  try {
+    const response = await axios.get(website);
+    const $ = cheerio.load(response.data);
+
+    const title = $('title').text();
+    const description = $('meta[name="description"]').attr('content');
+
+    return { title, description };
+  } catch (error) {
+    console.error(`Erreur lors du web scraping de ${website}:`, error);
+    return null;
+  }
+}
+
+// Fonction pour insérer les données scrapées dans la base de données
+function insertData(websiteData) {
+  if (websiteData === null) {
+    return;
+  }
+
+  const { title, description } = websiteData;
+  const query = 'INSERT INTO websites (title, description) VALUES (?, ?)';
+  connection.query(query, [title, description], (error, results) => {
+    if (error) {
+        console.error('Une erreur s est produite');
+    } else {
+        console.log('Données insérées avec succès !');
+    }
+    });
+}
+    
+// Fonction principale pour le scrapping de la liste des sites web
+async function scrapeWebsites() {
+
+    for (const website of webSiteList) {
+    const websiteData = await scrapeWebsite(website);
+    insertData(websiteData);
+    }
+    
+    // Fermeture de la connexion à la base de données
+    connection.end();
+}
+
+// Exécution de la fonction principale
+scrapeWebsites();
---
+++ listeWebSite.js
@@ -0,0 +1,31 @@
+const webSiteList = [
+    'https://kyliecosmetics.com/', 
+    'https://iliabeauty.com/',
+    'https://www.nastygal.com/',
+    'https://www.ae.com/',
+    'https://www.boohoo.com/',
+    'https://www.teasetea.com/',
+    'https://www.fashionphile.com/',
+    'https://www.prettylittlething.us/',
+    'https://www.farfetch.com/',
+    'https://auratenewyork.com/',
+    'https://www.ruelala.com/',
+    'https://johnscrazysocks.com/',
+    'https://www.ssense.com/',
+    'https://www.missguided.co.uk/',
+    'https://www.petitestudionyc.com/',
+    'https://www.cuyana.com/',
+    'https://www.rriveter.com/',
+    'https://www.theoutnet.com/',
+    'https://www.forever21.com/',
+    'https://reelpaper.com/',
+    'https://www.aesop.com/',
+    'https://www.youngandreckless.com/',
+    'https://www.freepeople.com/',
+    'https://www.target.com/',
+    'https://telfar.net/',
+    'https://www.senreve.com/'
+
+];
+
+module.exports = webSiteList
